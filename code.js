865d136df141906764041a78c7c5b2dff1f821c3
Hunter Parks <parks.hunter1992@gmail.com>
2021-02-09 13:44:36 -0600

    feat: intro to testing exercise

--- code.js
+++ code.js
@@ -1,4 +1,15 @@
 // helloWorld function 
-function helloWorld() {
-    return "Hello, World!";
+// function helloWorld() {
+//     return "Hello, World!";
+// }
+
+// const helloWorld = function() {
+//     return "Hello, World!";
+// }
+
+
+// sayHello function
+
+function sayHello(input){
+    return "Hello, " + input + "!";
 }
--- tests.js
+++ tests.js
@@ -1,15 +1,51 @@
-// Unit tests for the helloWorld function
-describe('helloWorld', function() {
-    it('should be a defined function', function() {
-      expect(typeof helloWorld).toBe('function');
+// // Unit tests for the helloWorld function
+// describe('helloWorld', function() {
+//     it('should be a defined function', function() {
+//       expect(typeof helloWorld).toBe('function');
+//     });
+//     it('should return a string when called', function() {
+//       expect(typeof helloWorld()).toBe("string");
+//     });
+//     it('should return the string "Hello, World!" when executed', function() {
+//       expect(helloWorld()).toBe("Hello, World!");
+//     });
+//     it("should never return 'undefined' when called", function() {
+//       expect(helloWorld()).not.toBe(undefined);
+//     });
+// });
+
+
+describe('sayHello',function(){
+    it('should be a defined function', function(){
+        expect(typeof sayHello).toBe('function');
     });
-    it('should return a string when called', function() {
-      expect(typeof helloWorld()).toBe("string");
+    it('should return a string when called', function(){
+        expect(typeof sayHello()).toBe("string");
     });
-    it('should return the string "Hello, World!" when executed', function() {
-      expect(helloWorld()).toBe("Hello, World!");
+    it('should return the string "say, Hello!" when executed', function(){
+        expect(sayHello("Pat")).toBe("Hello, Pat!");
     });
     it("should never return 'undefined' when called", function() {
-      expect(helloWorld()).not.toBe(undefined);
+        expect(sayHello()).not.toBe(undefined);
     });
-});
+
+    describe(isFive, function (){
+        it('should be a defined function', function(){
+            expect(typeof isFive).toBe('function');
+        })
+    })
+    it('should return a boolean no matter what input is',function(){
+        expect(isFive()).toBe("boolean");
+    });
+
+
+
+
+
+
+
+
+
+
+
+    })
